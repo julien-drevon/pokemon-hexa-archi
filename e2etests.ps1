c76cab67096fa19ef297e3f4c67aa44103f94e27
worm-rock <drevon.julien@gmail.com>
2022-11-12 01:55:38 +0100

    update ignore

--- e2etests.ps1
+++ e2etests.ps1
@@ -19,10 +19,3 @@ if (Test-Path "./postgresqldata"){
 }
 
 exit $result
-# docker build -t poke-test -f=DockerfileDev .
-# docker run poke-test
-# $result = $LASTEXITCODE
-#  if($result -eq 0){
-#  docker build -t poke-prod -f=DockerfileProd .
-#  docker run poke-prod
-# }
